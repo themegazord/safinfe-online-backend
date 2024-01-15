d19127223991f3391847fe9750a3603c8e68eb7f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-01-15 15:28:32 -0400

    wip

--- resources/views/livewire/componentes/formularios/input.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="{{$classGrupoInput ? $classGrupoInput : ''}}">
-    <label for="{{$for}}" class="{{$classLabel ? $classLabel : ''}}">{{$label}}</label>
-    <input type="{{$type}}" id="{{$for}}" class="{{$classInput ? $classInput : ''}}">
-</div>
